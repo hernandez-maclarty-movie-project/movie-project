0cdab5ed0784a1507ab38bc251afeac5a5079433
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-03-30 10:20:51 -0400

    Started on JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,61 @@
 "use strict";
-console.log(`Link Test`);
-console.log(`---------`);
-console.log();
 
-// URL
 
-console.log();
-console.log(`Exercise 1`);
-console.log(`------------`);
+// VARIABLES
+let glitchUrl = 'https://equal-factual-wallet.glitch.me/movies';
+let html = "";
+let movies;
+
+let omdbUrl = 'https://www.omdbapi.com';
+let omdbTitle;
+
+
+getDataGlitch();
+// getDataOmdb();
+
+
+function getDataGlitch() {
+    fetch(glitchUrl)
+        .then((response) => response.json())
+        .then((glitchData) => {
+
+            console.log(glitchData);
+
+            movies = document.querySelector(".list");
+
+            glitchData.forEach(function (movie) {
+
+                //language=HTML
+                html =
+                    `
+                        <div>Title: ${movie.title}</div>
+                    `
+                movies.innerHTML += html;
+
+            })
+        });
+}
+
+
+function addMovie() {
+
+}
+
+function editMovie() {
+
+}
+
+function rmMovie() {
+
+}
+
+
+function getDataOmdb() {
+    fetch(`${omdbUrl}/?apikey=${OMDB_KEY}&t=${omdbTitle}`)
+        .then((response) => response.json())
+        .then((omdgData) => {
+            console.log(omdbData);
+        });
+}
+
 
